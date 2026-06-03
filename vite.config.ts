61951484726c971b77f48b4d4dd528301c720d56
mithrangowda07 <mithrangowda01@gmail.com>
2026-06-04 00:03:42 +0530

    http->https

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,10 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://52.65.206.114:5000',
+        target: 'https://kcetapi.duckdns.org',
         changeOrigin: true,
       },
     },
   },
-  
+
 })
